6ca7ef437140f0b23f017423b0a1b23f43ddc1a6
Jay T <jaytula@gmail.com>
2021-05-12 09:56:38 -0700

    69. Adding Dynamic Styles

--- react-basics/src/components/Chart/ChartBar.tsx
+++ react-basics/src/components/Chart/ChartBar.tsx
@@ -9,7 +9,22 @@ const ChartBar = ({
   maxValue: number | null;
   label: string;
 }) => {
-  return <div className="chart-bar"></div>;
+  let barFillHeight = "0%";
+
+  if (maxValue !== null && maxValue > 0) {
+    barFillHeight = Math.round((value / maxValue) * 100) + "%";
+  }
+  return (
+    <div className="chart-bar">
+      <div className="chart-bar__inner">
+        <div
+          className="chart-bar__fill"
+          style={{ height: barFillHeight }}
+        ></div>
+      </div>
+      <div className="chart-bar__label">{label}</div>
+    </div>
+  );
 };
 
 export default ChartBar;
