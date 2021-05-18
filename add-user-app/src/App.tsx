14764ce43e57a4a049783d56d862b330c8582685
Jay T <jaytula@gmail.com>
2021-05-18 10:18:27 -0700

    Remove unneeded imports and logic in App.tsx

--- add-user-app/src/App.tsx
+++ add-user-app/src/App.tsx
@@ -2,28 +2,11 @@ import AddUser, { IUser } from "./components/Users/AddUser";
 import classes from "./App.module.css";
 import { useState } from "react";
 import UsersList from "./components/Users/UsersList";
-import ErrorModal, { IModalData } from "./components/UI/ErrorModal";
 
 function App() {
   const [usersList, setUsersList] = useState<IUser[]>([]);
-  const [modal, setModal] = useState<IModalData | null>(null);
 
   const addUserHandler = (user: IUser) => {
-    if (!user.name || !user.age) {
-      setModal({
-        title: "Invalid input",
-        message: "Please enter a valid name and age (non-empty values).",
-      });
-      return;
-    }
-
-    if (Number(user.age) < 0) {
-      setModal({
-        title: "Invalid input",
-        message: "Pease enter a valid age (> 0)",
-      });
-      return;
-    }
     setUsersList((prevUsersList) => [
       ...prevUsersList,
       { ...user, id: Math.random().toString() },
