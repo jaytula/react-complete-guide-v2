1be425c64efa62e6b19b38721446229119e13df9
Jay T <jaytula@gmail.com>
2021-05-20 13:31:23 -0700

    104. Working with Portals

--- add-user-app/src/components/UI/ErrorModal.tsx
+++ add-user-app/src/components/UI/ErrorModal.tsx
@@ -1,3 +1,5 @@
+import { Fragment } from "react";
+import { createPortal } from "react-dom";
 import Button from "./Button";
 import Card from "./Card";
 import classes from "./ErrorModal.module.css";
@@ -7,32 +9,54 @@ export interface IModalData {
   message: string;
 }
 
-const ErrorModal = ({
+const Backdrop = ({ onClose }: { onClose: () => void }) => {
+  return <div className={classes.backdrop} onClick={onClose} />;
+};
+
+const ModalOverlay = ({
   title,
   message,
-  onClose
+  onClose,
 }: {
   title: string;
   message: string;
   onClose: () => void;
 }) => {
-
-
   return (
-    <div>
-      <div className={classes.backdrop} onClick={onClose} />
-      <Card className={classes.modal}>
-        <header className={classes.header}>
-          <h2>{title}</h2>
-        </header>
-        <div className={classes.content}>
-          <p>{message}</p>
-        </div>
-        <footer className={classes.actions}>
-          <Button onClick={onClose}>Okay</Button>
-        </footer>
-      </Card>
-    </div>
+    <Card className={classes.modal}>
+      <header className={classes.header}>
+        <h2>{title}</h2>
+      </header>
+      <div className={classes.content}>
+        <p>{message}</p>
+      </div>
+      <footer className={classes.actions}>
+        <Button onClick={onClose}>Okay</Button>
+      </footer>
+    </Card>
+  );
+};
+
+const ErrorModal = ({
+  title,
+  message,
+  onClose,
+}: {
+  title: string;
+  message: string;
+  onClose: () => void;
+}) => {
+  return (
+    <Fragment>
+      {createPortal(
+        <Backdrop onClose={onClose} />,
+        document.getElementById("backdrop-root") as HTMLElement
+      )}
+      {createPortal(
+        <ModalOverlay title={title} message={message} onClose={onClose} />,
+        document.getElementById("overlay-root") as HTMLElement
+      )}
+    </Fragment>
   );
 };
 
