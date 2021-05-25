5edc4ea020e4e3c055c2d04f2f898d81ab474aec
Jay T <jaytula@gmail.com>
2021-05-25 10:01:47 -0700

    Add Log out button in Home component

--- advanced1/src/components/Home/Home.tsx
+++ advanced1/src/components/Home/Home.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from '../UI/Button/Button';
 
 import Card from '../UI/Card/Card';
 import classes from './Home.module.css';
@@ -7,6 +8,7 @@ const Home = ({onLogout}: {onLogout: () => void}) => {
   return (
     <Card className={classes.home}>
       <h1>Welcome back!</h1>
+      <Button onClick={onLogout}>Log out</Button>
     </Card>
   );
 };
--- advanced1/src/components/UI/Button/Button.tsx
+++ advanced1/src/components/UI/Button/Button.tsx
@@ -6,7 +6,7 @@ const Button = (props: {
   type?: "button" | "submit" | "reset";
   className?: string | undefined;
   onClick?: MouseEventHandler<HTMLButtonElement>;
-  disabled: boolean;
+  disabled?: boolean;
   children: ReactNode;
 }) => {
   return (
@@ -14,7 +14,7 @@ const Button = (props: {
       type={props.type || "button"}
       className={`${classes.button} ${props.className || ''}`}
       onClick={props.onClick}
-      disabled={props.disabled}
+      disabled={props.disabled || false}
     >
       {props.children}
     </button>
