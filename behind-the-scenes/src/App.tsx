868ea6ca4177b10da5c8edcddb92522154982ed3
Jay T <jaytula@gmail.com>
2021-06-15 13:56:40 -0700

    152. Component Updates in Action

--- behind-the-scenes/src/App.tsx
+++ behind-the-scenes/src/App.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import './App.css';
+import "./App.css";
+import Button from "./components/UI/Button/Button";
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState<boolean>(false);
+
+  console.log('App Running');
+  
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      <div>
+        <Button onClick={() => setShowParagraph((prev) => !prev)}>
+          Toggle Paragraph!
+        </Button>
+      </div>
+
+      {showParagraph && <p>This is new</p>}
     </div>
   );
 }
