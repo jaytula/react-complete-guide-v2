abd572e4e3bb852f0d3c287161bdd0d734af4fe6
Jay T <jaytula@gmail.com>
2021-06-16 16:41:37 -0700

    153. A Close Look at Child Component Re-Evaluation

--- behind-the-scenes/src/App.tsx
+++ behind-the-scenes/src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 
 import "./App.css";
+import DemoOutput from "./components/Demo/DemoOutput";
 import Button from "./components/UI/Button/Button";
 
 function App() {
@@ -17,7 +18,7 @@ function App() {
         </Button>
       </div>
 
-      {showParagraph && <p>This is new</p>}
+      <DemoOutput show={false} />
     </div>
   );
 }
