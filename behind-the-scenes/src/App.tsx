e5c2b00db0237821de3e3f30c65c69489e698391
Jay T <jaytula@gmail.com>
2021-06-17 14:27:47 -0700

    155. Preventing Function Re-Creation with useCallback()

--- behind-the-scenes/src/App.tsx
+++ behind-the-scenes/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 
 import "./App.css";
 import DemoOutput from "./components/Demo/DemoOutput";
@@ -7,15 +7,17 @@ import Button from "./components/UI/Button/Button";
 function App() {
   const [showParagraph, setShowParagraph] = useState<boolean>(false);
 
-  console.log('App Running');
-  
+  console.log("App Running");
+  const toggleParagraphHandler = useCallback(
+    () => setShowParagraph((prev) => !prev),
+    []
+  );
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
       <div>
-        <Button onClick={() => setShowParagraph((prev) => !prev)}>
-          Toggle Paragraph!
-        </Button>
+        <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
       </div>
 
       <DemoOutput show={false} />
