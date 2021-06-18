4823869c8e7535fbe307f06cb8b9d81a5d768420
Jay T <jaytula@gmail.com>
2021-06-18 14:55:36 -0700

    156. useCallback() and its Dependencies

--- behind-the-scenes/src/App.tsx
+++ behind-the-scenes/src/App.tsx
@@ -6,21 +6,26 @@ import Button from "./components/UI/Button/Button";
 
 function App() {
   const [showParagraph, setShowParagraph] = useState<boolean>(false);
+  const [allowToggle, setAllowToggle] = useState<boolean>(false);
 
   console.log("App Running");
-  const toggleParagraphHandler = useCallback(
-    () => setShowParagraph((prev) => !prev),
-    []
-  );
+  const toggleParagraphHandler = useCallback(() => {
+    if (allowToggle) setShowParagraph((prev) => !prev);
+  }, [allowToggle]);
+
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
+  };
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
       <div>
+        <Button onClick={allowToggleHandler}>Allow Toggling!</Button>
         <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
       </div>
 
-      <DemoOutput show={false} />
+      <DemoOutput show={showParagraph} />
     </div>
   );
 }
