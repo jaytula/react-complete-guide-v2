c2b7200b36409402afc84fa25eb8e0b5e436912c
Jay T <jaytula@gmail.com>
2021-06-21 14:05:15 -0700

    165. Working with State & Events

--- class-based/src/components/Users.tsx
+++ class-based/src/components/Users.tsx
@@ -1,37 +1,54 @@
-import { useState } from 'react';
-import User from './User';
+import { Component } from "react";
+import User from "./User";
 
-import classes from './Users.module.css';
+import classes from "./Users.module.css";
 
 const DUMMY_USERS = [
-  { id: 'u1', name: 'Max' },
-  { id: 'u2', name: 'Manuel' },
-  { id: 'u3', name: 'Julie' },
+  { id: "u1", name: "Max" },
+  { id: "u2", name: "Manuel" },
+  { id: "u3", name: "Julie" },
 ];
 
-const Users = () => {
-  const [showUsers, setShowUsers] = useState(true);
+interface MyProps {}
 
-  const toggleUsersHandler = () => {
-    setShowUsers((curState) => !curState);
-  };
+interface MyState {
+  showUsers: boolean;
+}
 
-  const usersList = (
-    <ul>
-      {DUMMY_USERS.map((user) => (
-        <User key={user.id} name={user.name} />
-      ))}
-    </ul>
-  );
+class Users extends Component<MyProps, MyState> {
+  // state = {
+  //   showUsers: true,
+  // };
+  constructor(props: MyProps, state: MyState) {
+    super(props, state);
+    this.state = {
+      showUsers: true
+    }
+  }
 
-  return (
-    <div className={classes.users}>
-      <button onClick={toggleUsersHandler}>
-        {showUsers ? 'Hide' : 'Show'} Users
-      </button>
-      {showUsers && usersList}
-    </div>
-  );
-};
+  toggleUsersHandler() {
+    this.setState((curState) => ({ showUsers: !curState.showUsers }));
+  }
+
+  render() {
+    const { showUsers } = this.state;
+    const usersList = (
+      <ul>
+        {DUMMY_USERS.map((user) => (
+          <User key={user.id} name={user.name} />
+        ))}
+      </ul>
+    );
+
+    return (
+      <div className={classes.users}>
+        <button onClick={this.toggleUsersHandler.bind(this)}>
+          {showUsers ? "Hide" : "Show"} Users
+        </button>
+        {showUsers && usersList}
+      </div>
+    );
+  }
+}
 
 export default Users;
