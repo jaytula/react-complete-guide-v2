f69a5e1c44e703729efb8b1d03c79e5c45d4fa05
Jay T <jaytula@gmail.com>
2021-06-27 12:00:50 -0700

    176. Using async/await

--- http-requests/src/App.tsx
+++ http-requests/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
@@ -7,43 +7,25 @@ import { IMovie } from "./models";
 function App() {
   const [movies, setMovies] = useState<IMovie[]>([]);
 
-  function fetchMoviesHandler() {
-    fetch("https://swapi.dev/api/films")
-      .then(res => res.json())
-      .then(
-        (data: {
-          results: {
-            episode_id: number;
-            title: string;
-            openingCrawl: string;
-            release_date: string;
-          }[];
-        }) => {
-          const fetchedMovies = data.results.map(item => ({
-            id: item.episode_id,
-            title: item.title,
-            openingText: item.openingCrawl,
-            releaseDate: item.release_date,
-          }));
-          setMovies(fetchedMovies);
-        }
-      );
-  }
+  async function fetchMoviesHandler() {
+    const response = await fetch("https://swapi.dev/api/films");
+    const data: {
+      results: {
+        episode_id: number;
+        title: string;
+        openingCrawl: string;
+        release_date: string;
+      }[];
+    } = await response.json();
 
-  const dummyMovies: IMovie[] = [
-    {
-      id: 1,
-      title: "Some Dummy Movie",
-      openingText: "This is the opening text of the movie",
-      releaseDate: "2021-05-18",
-    },
-    {
-      id: 2,
-      title: "Some Dummy Movie 2",
-      openingText: "This is the second opening text of the movie",
-      releaseDate: "2021-05-19",
-    },
-  ];
+    const fetchedMovies = data.results.map(item => ({
+      id: item.episode_id,
+      title: item.title,
+      openingText: item.openingCrawl,
+      releaseDate: item.release_date,
+    }));
+    setMovies(fetchedMovies);
+  }
 
   return (
     <React.Fragment>
