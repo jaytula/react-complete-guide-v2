1dfea130521acb6c3345b810242d69629992dbfc
Jay T <jaytula@gmail.com>
2021-07-03 10:05:13 -0700

    180. Preparing The Project For the next steps

--- sending-post-request/src/App.js
+++ sending-post-request/src/App.js
@@ -13,7 +13,7 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch('https://swapi.dev/api/films/');
+      const response = await fetch(`${process.env.REACT_APP_FIREBASE_BACKEND}/movies.json`);
       if (!response.ok) {
         throw new Error('Something went wrong!');
       }
