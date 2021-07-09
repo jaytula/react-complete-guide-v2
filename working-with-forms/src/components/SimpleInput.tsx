5f646ce805030d5a8e64885a1b0248b1a1adba12
Jay T <jaytula@gmail.com>
2021-07-09 14:25:37 -0700

    198. Dealing With Form Submission & Getting User Input Values

--- working-with-forms/src/components/SimpleInput.tsx
+++ working-with-forms/src/components/SimpleInput.tsx
@@ -1,9 +1,39 @@
+import { useRef } from "react";
+import { FormEventHandler } from "react";
+import { ChangeEventHandler, useState } from "react";
+
 const SimpleInput = () => {
+  const nameInputRef = useRef<HTMLInputElement>(null);
+  const [enteredName, setEnteredName] = useState<string>("");
+
+  const nameInputChangeHandler: ChangeEventHandler<HTMLInputElement> = (
+    event
+  ) => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmissionHandler: FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+
+    console.log(enteredName);
+    const enteredValue = nameInputRef.current?.value;
+    console.log(enteredValue);
+
+    // nameInputRef?.current?.value = ''; // Bad: directly manipulating the DOM
+    setEnteredName('');
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmissionHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          type="text"
+          id="name"
+          value={enteredName}
+          onChange={nameInputChangeHandler}
+          ref={nameInputRef}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
