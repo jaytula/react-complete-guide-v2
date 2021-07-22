c088b6cf48c569017f7657c28ff1769c79ef1b43
Jay T <jaytula@gmail.com>
2021-07-22 13:39:45 -0700

    210: Bonus:L Using useReducer()

--- working-with-forms/src/hooks/use-input.tsx
+++ working-with-forms/src/hooks/use-input.tsx
@@ -1,30 +1,56 @@
-import { ChangeEventHandler, FocusEventHandler, useState } from "react";
+import { ChangeEventHandler, FocusEventHandler, useReducer } from "react";
 
+type InputState = {
+  value: string;
+  touched: boolean;
+};
+
+const inputStateReducer = (
+  state: InputState,
+  action: {
+    type: "SET_VALUE" | "SET_TOUCHED";
+    value?: string;
+    touched?: boolean;
+  }
+) => {
+  if (action.type === "SET_VALUE" && typeof action.value === "string") {
+    return { ...state, value: action.value };
+  }
+
+  if (action.type === "SET_TOUCHED" && typeof action.touched === "boolean") {
+    return { ...state, touched: action.touched };
+  }
+
+  return state;
+};
 const useInput = (
   name: string,
   label: string,
   type: string,
   validator: (value: string) => boolean
 ) => {
-  const [value, setValue] = useState<string>("");
-  const [touched, setTouched] = useState<boolean>(false);
+  const [inputState, dispatch] = useReducer(inputStateReducer, {
+    value: "",
+    touched: false,
+  });
+  const { value, touched } = inputState;
 
   const isValid = validator(value);
   const inputIsInvalid = !isValid && touched;
 
   const inputClasses = inputIsInvalid ? "form-control invalid" : "form-control";
 
-  const inputChangeHandler: ChangeEventHandler<HTMLInputElement> = event => {
-    setValue(event.target.value);
+  const inputChangeHandler: ChangeEventHandler<HTMLInputElement> = (event) => {
+    dispatch({ type: "SET_VALUE", value: event.target.value });
   };
 
-  const inputBlurHandler: FocusEventHandler<HTMLInputElement> = event => {
-    setTouched(true);
+  const inputBlurHandler: FocusEventHandler<HTMLInputElement> = (event) => {
+    dispatch({ type: "SET_TOUCHED", touched: true });
   };
 
   const reset = () => {
-    setValue("");
-    setTouched(false);
+    dispatch({ type: "SET_VALUE", value: "" });
+    dispatch({ type: "SET_TOUCHED", touched: false });
   };
 
   const inputElem = (
