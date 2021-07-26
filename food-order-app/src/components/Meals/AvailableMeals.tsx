4dd9c22f7a98a889710d1077b57206ee966e00cc
Jay T <jaytula@gmail.com>
2021-07-26 12:26:40 -0700

    214. Fetching Meals via Http

--- food-order-app/src/components/Meals/AvailableMeals.tsx
+++ food-order-app/src/components/Meals/AvailableMeals.tsx
@@ -1,33 +1,7 @@
+import { useEffect, useState } from "react";
 import Card from "../UI/Card";
 import classes from "./AvailableMeals.module.css";
 import MealItem from "./MealItem/MealItem";
-
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
 
 export interface IMealItem {
   id: string;
@@ -37,9 +11,25 @@ export interface IMealItem {
 }
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
-    <MealItem key={meal.id} item={meal} />
-  ));
+  const [meals, setMeals] = useState<IMealItem[]>([]);
+
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_FIREBASE_BACKEND}/meals.json`)
+      .then((res) => res.json())
+      .then(
+        (data: {
+          [key: string]: { name: string; description: string; price: number };
+        }) => {
+          const fetchedMeals = Object.entries(data).map(([key, value]) => ({
+            id: key,
+            ...value
+          }))
+          setMeals(fetchedMeals);
+        }
+      );
+  }, []);
+
+  const mealsList = meals.map((meal) => <MealItem key={meal.id} item={meal} />);
 
   return (
     <section className={classes.meals}>
