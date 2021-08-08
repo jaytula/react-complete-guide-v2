ecf9f10bb1e160ccdf350a86902801d9e24ee68d
Jay T <jaytula@gmail.com>
2021-08-08 11:35:35 -0700

    218. Reading Form Values

--- food-order-app/src/components/Cart/Checkout.tsx
+++ food-order-app/src/components/Cart/Checkout.tsx
@@ -1,32 +1,52 @@
-import { FormEventHandler } from "react";
+import { FormEventHandler, useRef } from "react";
 import classes from "./Checkout.module.css";
 
 const Checkout = ({ onCancel }: { onCancel: () => void }) => {
-  const confirmHandler: FormEventHandler<HTMLFormElement> = (event) => {
+  const nameInputRef = useRef<HTMLInputElement>(null);
+  const streetInputRef = useRef<HTMLInputElement>(null);
+  const postalCodeInputRef = useRef<HTMLInputElement>(null);
+  const cityInputRef = useRef<HTMLInputElement>(null);
+
+  const confirmHandler: FormEventHandler<HTMLFormElement> = event => {
     event.preventDefault();
+
+    const enteredName = nameInputRef?.current?.value as string;
+    const enteredStreet = streetInputRef?.current?.value as string;
+    const enteredPostalCode = postalCodeInputRef?.current?.value as string;
+    const enteredCity = cityInputRef?.current?.value as string;
+
+    console.log({
+      enteredName,
+      enteredStreet,
+      enteredPostalCode,
+      enteredCity,
+    });
   };
+
   return (
     <form onSubmit={confirmHandler}>
       <div className={classes.control}>
         <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" />
+        <input type="text" id="name" ref={nameInputRef} />
       </div>
       <div className={classes.control}>
         <label htmlFor="street">Street</label>
-        <input type="text" id="street" />
+        <input type="text" id="street" ref={streetInputRef} />
       </div>
       <div className={classes.control}>
         <label htmlFor="postal">Postal Code</label>
-        <input type="text" id="postal" />
+        <input type="text" id="postal" ref={postalCodeInputRef} />
       </div>
       <div className={classes.control}>
         <label htmlFor="city">City</label>
-        <input type="text" id="city" />
+        <input type="text" id="city" ref={cityInputRef} />
       </div>
-      <button type="button" onClick={onCancel}>
-        Cancel
-      </button>
-      <button>Confirm</button>
+      <div className={classes.actions}>
+        <button type="button" onClick={onCancel}>
+          Cancel
+        </button>
+        <button>Confirm</button>
+      </div>
     </form>
   );
 };
