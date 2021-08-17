5d5a0922f16b3c5a71e733752418974d987416eb
Jay T <jaytula@gmail.com>
2021-08-17 01:10:09 -0700

    229. More Redux Basics

--- explore-redux-concepts/src/redux-demo.ts
+++ explore-redux-concepts/src/redux-demo.ts
@@ -1,16 +1,28 @@
-import {createStore} from 'redux'
+import { createStore } from "redux";
 
-const counterReducer = (state={counter: 0}, action:{type: string}) => {
-  return state
-}
+const counterReducer = (
+  state = { counter: 0 },
+  action: { type: "increment" | "decrement" | "unknown" }
+) => {
+  switch (action.type) {
+    case "increment":
+      return { counter: state.counter + 1 };
+    case "decrement":
+      return { counter: state.counter - 1 };
+  }
+  return state;
+};
 
 const store = createStore(counterReducer);
 
 const counterSubscriber = () => {
-  const latestState = store.getState()
+  const latestState = store.getState();
   console.log(latestState);
-}
+};
 
 store.subscribe(counterSubscriber);
 
-store.dispatch({type: 'increment'})
+store.dispatch({ type: "increment" });
+store.dispatch({ type: "increment" });
+store.dispatch({ type: "increment" });
+store.dispatch({ type: "decrement" });
