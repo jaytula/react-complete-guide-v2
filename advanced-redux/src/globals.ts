d95214cdf357ffd38939f90497f04319e0d2aa4a
Jay T <jaytula@gmail.com>
2021-09-12 17:05:38 -0700

    Rename FIRESTORE to FIREBASE

--- advanced-redux/src/globals.ts
+++ advanced-redux/src/globals.ts
@@ -1,7 +1,7 @@
-export const FIRESTORE_BACKEND = process.env.REACT_APP_FIRESTORE_BACKEND as string;
+export const FIREBASE_BACKEND = process.env.REACT_APP_FIREBASE_BACKEND as string;
 
-if(typeof FIRESTORE_BACKEND !== 'string' || !FIRESTORE_BACKEND) {
-  throw new Error('FIRESTORE_BACKEND not set');
+if(typeof FIREBASE_BACKEND !== 'string' || !FIREBASE_BACKEND) {
+  throw new Error('FIREBASE_BACKEND not set');
 }
 
 console.log('xyz');
