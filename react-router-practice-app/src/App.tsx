ccc0b77e7dcf10d979f433b00b3f1d0ab2c5fe84
Jay T <jaytula@gmail.com>
2021-09-21 17:15:39 -0700

    275. Time to Practice: Onwards to a New Project

--- react-router-practice-app/src/App.tsx
+++ react-router-practice-app/src/App.tsx
@@ -1,9 +1,32 @@
+import { Redirect, Route, Switch } from "react-router";
+import NewQuotePage from "./pages/NewQuote";
+import QuoteDetailPage from "./pages/QuoteDetail";
+import QuotesListPage from "./pages/AllQuotes";
+
 function App() {
   return (
     <div>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/quotes" />
+        </Route>
 
+        <Route path="/quotes" exact>
+          <QuotesListPage />
+        </Route>
+        <Route path="/quotes/:quoteId">
+          <QuoteDetailPage />
+        </Route>
+        <Route path="/new-quote">
+          <NewQuotePage />
+        </Route>
+      </Switch>
     </div>
   );
 }
 
+// /quotes/:quoteId
+// /quotes/:quoteId/comments
+// /quotes
+// /new-quote
 export default App;
