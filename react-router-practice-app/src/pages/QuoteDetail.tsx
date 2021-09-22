64324b76d98c03511857a4c17e510616522762e0
Jay T <jaytula@gmail.com>
2021-09-22 01:01:38 -0700

    276. Practicing Redirecting & Extracting Params

--- react-router-practice-app/src/pages/QuoteDetail.tsx
+++ react-router-practice-app/src/pages/QuoteDetail.tsx
@@ -1,5 +1,17 @@
+import { Fragment } from "react";
+import { Route, useParams } from "react-router";
+import Comments from "../components/comments/Comments";
+
 const QuoteDetail = () => {
-  return <h1>QuoteDetail</h1>
+  const {quoteId} = useParams<{quoteId: string}>()
+  return <Fragment>
+    <h1>QuoteDetail</h1>
+    <p>{quoteId}</p>
+
+    <Route path={`/quotes/${quoteId}/comments`}>
+      <Comments />
+    </Route>
+  </Fragment>
 }
 
 export default QuoteDetail;
